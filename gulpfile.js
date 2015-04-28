0ea63cb45f8280841045f988e2261ed17bf25a0f
henk <henkiexxx4@gmail.com>
2015-04-28 15:07:07 +0200

    Added gulp connect module, and create grunt watch tasks for watching changes in html files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp  = require('gulp');
 var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var connect = require('gulp-connect');
+var files = ['./styles/**/*.scss', 'index.html'];
 
 gulp.task('styles', function() {
   gulp.src('styles/*.scss')
@@ -10,12 +12,27 @@ gulp.task('styles', function() {
       .pipe(autoprefixer('last 2 version'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('styles'))
+    .pipe(connect.reload())
+});
+
+gulp.task('html', function () {
+  gulp.src('*.html')
+    .pipe(connect.reload())
+});
+
+gulp.task('css', function () {
+  gulp.src('styles/application.css')
+    .pipe(connect.reload())
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./styles/**/*.scss', ['styles']);
+  gulp.watch('./styles/**/*.scss', ['css']);
+  gulp.watch('*.html', ['html']);
 });
 
-gulp.task('build', ['styles']);
+gulp.task('connect', function() {
+  connect.server({livereload: true});
+})
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('build', ['styles']);
+gulp.task('default', ['build', 'connect', 'watch']);
